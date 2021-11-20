06bdbdc70e228232e66bbeb9a76f9c3fea1d40d2
net <wendell20904102@gmail.com>
2021-11-20 22:21:16 +0800

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+
+//     if(reg.installing) {
+//       console.log('Service worker installing');
+//     } else if(reg.waiting) {
+//       console.log('Service worker installed');
+//     } else if(reg.active) {
+//       console.log('Service worker active');
+//     }
+
+//   }).catch(function(error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
+
+if ('serviceWorker' in navigator) {
+  ;(async function () {
+    try {
+      console.log('new 4')
+      // navigator.serviceWorker.getRegistrations().then((regs) => {
+      //   regs.forEach((reg) => {
+      //     reg.unregister()
+      //   })
+      // })
+      const reg = await navigator.serviceWorker.register('sw2.js')
+    } catch (e) {
+      console.error(e)
+    }
+  })()
+}
---
+++ sw.js
@@ -0,0 +1,31 @@
+const version = 'v1'
+const filenames = ['/index.html', '/style.css', '/app.js']
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(addCaches(version, filenames))
+})
+self.addEventListener('activate', function (event) {
+  event.waitUntil(deleteOldCaches(version))
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(getFile(event))
+})
+
+async function addCaches(version, filenames) {
+  const cache = await caches.open(version)
+  await cache.addAll(filenames)
+}
+
+async function getFile(event) {
+  const response = await caches.match(event.request)
+  return response || fetch(event.request)
+}
+
+async function deleteOldCaches(version) {
+  const keyList = await caches.keys()
+  const deletePromises = keyList.map(
+    (key) => key !== version && caches.delete(key)
+  )
+  return Promise.all(deletePromises)
+}
